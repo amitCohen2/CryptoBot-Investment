9ca0482bc6346a6db91f6ef1874d3b265b151cf7
amitCohen2 <amitcohen435@gmail.com>
2023-07-09 18:55:47 +0300

    update arbitrage

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -18,13 +18,14 @@ def create_app():
     from .views import views
     from .auth import auth
     from .twitter import twitter
-    from .arbitrage import arbitrage_blue_print
+    from .arbitrage import arbitrage_blue_print, arbitrage_inputs
 
     # Register the blueprints - this is where the routes are defined
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(twitter, url_prefix='/')
     app.register_blueprint(arbitrage_blue_print, url_prefix='/')
+    app.register_blueprint(arbitrage_inputs, url_prefix='/')
 
     from .modules import User, Note
 
--- website/arbitrage.py
+++ website/arbitrage.py
@@ -1,12 +1,24 @@
 import ccxt
 from datetime import datetime
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from flask_login import login_required
+import os
+import signal
+
+
+'''
+global const variables
+'''
 
 KRAKEN_BUY_FEE, KRAKEN_SELL_FEE = 0.0016, 0.0026
 BITSMAP_BUY_FEE, BITSMAP_SELL_FEE = 0.003, 0.004
-FLAT_FEE = 0.001  # BINANCE, 0.5 GATE, KUCOIN
+FLAT_FEE = 0.001  # BINANCE, KUCOIN
+NUM_OF_STOCK_EXCHANGES = 4
 
+
+'''
+    Counter class for ordering the arbitrage transactions and sum
+'''
 
 
 class Counter:
@@ -17,6 +29,11 @@ class Counter:
         Counter.self += value
         Counter.arbitrage_number += 1
         print('counter is ', round(Counter.self, 4))
+
+
+'''
+    Class for ordering the arbitrage format
+'''
 
 
 class ArbitrageFormat:
@@ -32,19 +49,23 @@ class ArbitrageFormat:
         self.counter = Counter.self
 
 
-# to store count of objects created
+'''
+    to store count of objects created
+'''
+
+
 def arbitrage(exchange1, exchange2, symbol, Counter, arbitrage_output):
     # Get the order book for the two exchanges
-    if ((exchange1 == gate or exchange1 == kraken or exchange1 == bitstamp) and
+    if ((exchange1 == kraken or exchange1 == bitstamp) and
             (exchange2 == kraken or exchange2 == bitstamp)):
         new_symbol = str(symbol[:-1])
         order_book1 = exchange1.fetch_ticker(new_symbol)
         order_book2 = exchange2.fetch_ticker(new_symbol)
-    elif (exchange1 == gate) or (exchange1 == kraken) or (exchange1 == bitstamp):
+    elif (exchange1 == kraken) or (exchange1 == bitstamp):
         new_symbol = str(symbol[:-1])
         order_book1 = exchange1.fetch_ticker(new_symbol)
         order_book2 = exchange2.fetch_ticker(symbol)
-    elif (exchange2 == gate) or (exchange2 == kraken) or (exchange2 == bitstamp):
+    elif (exchange2 == kraken) or (exchange2 == bitstamp):
         new_symbol = str(symbol[:-1])
         order_book1 = exchange1.fetch_ticker(symbol)
         order_book2 = exchange2.fetch_ticker(new_symbol)
@@ -66,7 +87,7 @@ def arbitrage(exchange1, exchange2, symbol, Counter, arbitrage_output):
     opportunity2 = bid_price1 - ask_price2  # calculate_with_fee(bid_price1, ask_price2, exchange1, exchange2)
     if opportunity1 > 0:
         # Calculate the amount of cryptocurrency to buy and sell
-        if (my_wallet[str(exchange2)]["USDT"] < ask_price1):
+        if my_wallet[str(exchange2)]["USDT"] < ask_price1:
             transform_money(exchange2, exchange1, ask_price1, my_wallet)
 
         amount = min(ask_price1, bid_price2)
@@ -84,17 +105,15 @@ def arbitrage(exchange1, exchange2, symbol, Counter, arbitrage_output):
         for market in my_wallet:
             total += my_wallet[str(market)]["USDT"]
         print(total)
-        # exchange1.create_order(symbol, 'limit', 'buy', amount, ask_price1)
-        # exchange2.create_order(symbol, 'limit', 'sell', amount, bid_price2)
+
     if opportunity2 > 0:
-        if (my_wallet[str(exchange1)]["USDT"] < ask_price2):
+        if my_wallet[str(exchange1)]["USDT"] < ask_price2:
             transform_money(exchange1, exchange2, ask_price2, my_wallet)
         # Calculate the amount of cryptocurrency to buy and sell
         amount = min(bid_price1, ask_price2)
 
         Counter.add(Counter.self, opportunity2)
         arbitrage_output.append(ArbitrageFormat(symbol, ask_price2, exchange1, bid_price1, exchange2, opportunity2))
-
         print(
            'Opportunity found: Buy {} for {}$ in {} and sell it on {} in {} '
            ',neto profit is {}$ (include fees)'.format(symbol, ask_price2, exchange1, bid_price1, exchange2,
@@ -107,11 +126,8 @@ def arbitrage(exchange1, exchange2, symbol, Counter, arbitrage_output):
             total += my_wallet[str(market)]["USDT"]
         print(total)
 
-        # Place the buy and sell orders
-        # exchange1.create_order(symbol, 'limit', 'buy', amount, ask_price1)
-        # exchange2.create_order(symbol, 'limit', 'sell', amount, bid_price2)
 
-
+# transform money if there is an arbitrage and the exchange wallet has no money
 def transform_money(buy_market, sell_market, ask_price1, my_wallet):
     out = False
     if my_wallet[str(sell_market)]["USDT"] >= ask_price1:
@@ -124,7 +140,7 @@ def transform_money(buy_market, sell_market, ask_price1, my_wallet):
                 my_wallet[str(market)]["USDT"] -= ask_price1
                 out = True
                 break
-        if (out == False):
+        if out == False:
             for market in my_wallet:
                 if my_wallet[str(market)]["USDT"] > 0 and my_wallet[str(buy_market)]["USDT"] < ask_price1 \
                         and market != buy_market:
@@ -145,8 +161,6 @@ def calculate_with_fee(sell_price, buy_price, buy_platform, sell_platform):
         buy_fees = buy_price * KRAKEN_BUY_FEE
     elif buy_platform == bitstamp:
         buy_fees = buy_price * BITSMAP_BUY_FEE
-    elif buy_platform == gate:
-        buy_fees = buy_price * FLAT_FEE * 2
     else:
         return 0
 
@@ -157,8 +171,6 @@ def calculate_with_fee(sell_price, buy_price, buy_platform, sell_platform):
         return (bruto_profit * (1 - KRAKEN_SELL_FEE)) - buy_fees
     elif sell_platform == bitstamp:
         return (bruto_profit * (1 - BITSMAP_SELL_FEE)) - buy_fees
-    elif buy_platform == gate:
-        return (bruto_profit * (1 - 2 * FLAT_FEE)) - buy_fees
     return 0
 
 
@@ -173,42 +185,83 @@ def calculate_buying_precent(ask_price, bid_price, ask_market, my_wallet):
 
 # Create the exchanges
 binance = ccxt.binance()
-gate = ccxt.gate()
 kraken = ccxt.kraken()
 kucoin = ccxt.kucoin()
 bitstamp = ccxt.bitstamp()
 binance.load_markets()
-gate.load_markets()
 kraken.load_markets()
 kucoin.load_markets()
 bitstamp.load_markets()
 
-markets = [binance, gate, kraken, kucoin, bitstamp]
-binance_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 10000.0}
-gate_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 10000.0}
-kraken_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 10000.0}
-kucoin_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 10000.0}
-bitstamp_wallet = {"BTC": 0.1, "ETH": 0.1, "LTC": 1.0, "USDT": 10000.0}
-my_wallet = {"Binance": binance_wallet, "Gate.io": gate_wallet, "Kraken": kraken_wallet, "KuCoin": kucoin_wallet, "Bitstamp": bitstamp_wallet}
-symbol = ['BTC/USDT', 'ETH/USDT', 'LTC/USDT']
-
-
 arbitrage_blue_print = Blueprint('arbitrage', __name__)
+arbitrage_inputs = Blueprint('arbitrage_inputs', __name__)
+
+
+@arbitrage_inputs.route('/arbitrage-input')
+def arbitrage_input():
+    return render_template('arbitrage-input.html')
+
+
+'''
+    init global variables for markets, wallets, symbols 
+'''
+markets = [binance, kraken, kucoin, bitstamp]
+binance_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 0.0}
+kraken_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 0.0}
+kucoin_wallet = {"BTC": 0.1, "ETH": 0.5, "LTC": 1.0, "USDT": 0.0}
+bitstamp_wallet = {"BTC": 0.1, "ETH": 0.1, "LTC": 1.0, "USDT": 0.0}
+my_wallet = {"Binance": binance_wallet,
+             "Kraken": kraken_wallet,
+             "KuCoin": kucoin_wallet,
+             "Bitstamp": bitstamp_wallet}
+symbol = ['BTC/USDT', 'ETH/USDT', 'LTC/USDT']
+stock_amount = 0.0
 
 
 # Run the arbitrage algorithm every minute
 @arbitrage_blue_print.route('/arbitrage', methods=['GET', 'POST'])
 @login_required
 def run_arbitrage():
-    global symbol
     arbitrage_output = []
+    init_wallets_from_user_input()
+    global symbol
     while True:
         for exchange1 in markets:
             for exchange2 in markets:
                 if exchange1 != exchange2:
                     for coin in symbol:
                         arbitrage(exchange1, exchange2, coin, Counter, arbitrage_output)
-        # time.sleep(60)
         return render_template('arbitrage.html', output=arbitrage_output)
 
 
+def init_wallets_from_user_input():
+    global stock_amount
+    is_user_insert_input = False
+
+    if stock_amount == 0.0:
+        is_user_insert_input = True
+        stock_amount = get_user_amount_input() / NUM_OF_STOCK_EXCHANGES
+    else:
+        try:
+            stock_amount = get_user_amount_input() / NUM_OF_STOCK_EXCHANGES
+            Counter.self = 0
+            Counter.arbitrage_number = 0
+            is_user_insert_input = True
+        except:
+            print("The User doesn't enter a new invest input!")
+
+    if is_user_insert_input:
+        binance_wallet["USDT"] = stock_amount
+        kraken_wallet["USDT"] = stock_amount
+        kucoin_wallet["USDT"] = stock_amount
+        bitstamp_wallet["USDT"] = stock_amount
+
+
+def get_user_amount_input():
+    return int(request.form['amount'])
+
+
+@arbitrage_blue_print.route('/arbitrage', methods=['GET'])
+def stop_python_script():
+    os.kill(os.getpid(), signal.SIGINT)
+    return 'Python script stopped.'
